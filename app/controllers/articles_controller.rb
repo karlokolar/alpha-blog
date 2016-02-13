82eff86120dd1b776def3dbb3f0ce4a78986feb6
Karlo Kolar <alien@kolar.foundation>
2016-02-13 14:17:24 +0100

    Edit, show and stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,24 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully update!"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
