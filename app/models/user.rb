4164bd7040a153d60c3c5c27fc1f43a110894cba
Karlo Kolar <alien@kolar.foundation>
2016-02-16 08:25:28 +0100

    Add secure password!

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ActiveRecord::Base
             length: {maximum: 48},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
-
+  has_secure_password
 end
