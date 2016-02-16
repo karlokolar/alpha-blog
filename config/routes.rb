c775f0acd6a1dee386b306012e5273ee8204e301
Karlo Kolar <alien@kolar.foundation>
2016-02-16 09:12:04 +0100

    Signup new users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "about", to: "pages#about"
+  resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -18,7 +21,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :articles
+
 
   # Example resource route with options:
   #   resources :products do
